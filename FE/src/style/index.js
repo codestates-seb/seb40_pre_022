51924ba2986fdacf3e5973c4d61a7f9a3f8093f5
MINDA01 <ekdud4639@gmail.com>
2022-10-26 16:00:10 +0900

    feat: 공통styles 파일 추가 및 안쓰는 css 파일 정리

---
+++ FE/src/style/index.js
@@ -0,0 +1,30 @@
+import { css } from "styled-components";
+
+export const flexCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const flexRowBetween = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const flexColumnCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const flexRowCenter = css`
+  display: flex;
+  align-items: center;
+`;
+
+export const flexColumn = css`
+  display: flex;
+  flex-direction: column;
+`;
