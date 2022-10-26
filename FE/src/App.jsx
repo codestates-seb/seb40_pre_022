af38470a2467254f79cb5dee825b866fe9e0a200
hongdahee <heewhy3513@gmail.com>
2022-10-26 11:39:35 +0900

    feat: router 추가

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,32 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Login from './pages/Login';
+import Home from './pages/Home'
+import Join from './pages/Join'
+import MyPage from './pages/MyPage'
+import QuestionAsk from './pages/QuestionAsk'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/join" element={<Join />}></Route>
+      <Route path="/question/ask" element={<QuestionAsk />}></Route>
+      <Route path="/mypage" element={<MyPage />}></Route>
+     </Routes>
+     </BrowserRouter>
     </div>
   )
 }
