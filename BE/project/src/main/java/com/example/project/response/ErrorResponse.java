f04ad0686d5726cb0bf07ba8dacf1d05fc2b97b9
jisu <jisuuoo96@gmail.com>
2022-10-27 09:54:23 +0900

    feat: 기본생성자를 private 로 수정

--- BE/project/src/main/java/com/example/project/response/ErrorResponse.java
+++ BE/project/src/main/java/com/example/project/response/ErrorResponse.java
@@ -5,10 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class ErrorResponse {
     private int status;
     private String message;
+
+    private ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 
     public static ErrorResponse of(ExceptionCode exceptionCode) {
         return new ErrorResponse(exceptionCode.getStatus(), exceptionCode.getMessage());
