bd1679e99be0f97a0aff13e998b947b05b6499c8
MINDA01 <ekdud4639@gmail.com>
2022-11-02 14:17:13 +0900

    feat: recovery page 퍼블작업 완료

---
+++ FE/src/pages/Recovery/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from "react";
+
+import { EMAIL_REGEX } from '../../constants/regex';
+
+import Layout from '@components/Layout';
+import TextInput from '@components/TextInput';
+import { Button } from '@components/Button';
+
+import { Wrapper, FormWrap } from './style';
+
+const Recovery = () => {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(false);
+
+  const handleChangeEmail = useCallback ((e)=> {
+      if(EMAIL_REGEX.test(e.target.value)){
+        setEmailError(false);
+      }
+      setEmail(e.target.value)
+  }, [email])
+
+
+  const handleSubmit = ((e)=>{
+    e.preventDefault();
+    if (!EMAIL_REGEX.test(email)) {
+      if (!EMAIL_REGEX.test(email)) setEmailError(true);
+      return;
+    }
+  })
+
+  return (
+    <Layout isAside={false}>
+      <Wrapper>
+          <FormWrap>
+            <form>
+              <p>Forgot your account’s password or having trouble logging into your Team? Enter your email address and we’ll send you a recovery link.</p>
+              <TextInput id='email' label='Email' errorMsg='이메일 형식을 맞춰주세요.' isError={emailError} value={email} onChange={handleChangeEmail} />
+              <Button label='Send recovery email' type='submit' onClick={handleSubmit}>Send recovery email</Button>
+            </form>
+          </FormWrap>
+      </Wrapper>
+    </Layout>
+  )
+};
+
+export default Recovery;
