e7f662d85abd6313dea6d92f0bd711b9b809bb73
Helperbot5 <eswirdtheo@gmail.com>
2022-11-02 11:01:33 +0900

    feat: contants 파일 생성

--- FE/src/components/Accordian/index.jsx
+++ FE/src/components/Accordian/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { step1_1 } from "../../constants/accordian";
 
 import {
   AskRightAside,
@@ -11,27 +12,6 @@ import {
   Ol,
   Ul,
 } from "./style";
-
-const step1_1 = [
-  {
-    title: "1. Summarize the problem",
-    content: [
-      "Include details about your goal\nDescribe expected and actual results\nInclude any error messages",
-    ],
-  },
-  {
-    title: "2. Describe what you've tried",
-    content: [
-      "IShow what you’ve tried and tell us what you found (on this site or elsewhere) and why it didn’t meet your needs. You can get better answers when you provide research.",
-    ],
-  },
-  {
-    title: "3. show some code",
-    content: [
-      "When appropriate, share the minimum amount of code others need to reproduce your problem (also called a minimum, reproducible example)",
-    ],
-  },
-];
 
 const Accordian = () => {
   const [select, setSelect] = useState(false);
